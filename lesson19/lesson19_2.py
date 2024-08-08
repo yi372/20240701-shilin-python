bed826021290cc8029c6d5d7df1350713787a6ea
yi372 <melody20050924@gmail.com>
2024-08-08 08:30:01 +0000

    modify

---
+++ lesson19/lesson19_2.py
@@ -0,0 +1,8 @@
+from tools import get_path,station_info,merge_dataFrame
+import pandas as pd
+if __name__ == "__main__":
+    fileName_list = get_path()
+    stations_df = station_info()
+    all_in_out = merge_dataFrame(fileName_list, stations_df)
+    station_in_out= pd.concat(all_in_out)
+    print(station_in_out)
